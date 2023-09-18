6fce9d71f809c4621ee421704620f7631a39e9f9
Ruby <puczekdragon@gmail.com>
2023-09-18 17:33:31 +0200

    added super basic Redux store

--- src/ts/App.tsx
+++ src/ts/App.tsx
@@ -1,7 +1,10 @@
+import { Counter } from "./features/counter/Counter";
+
 function App() {
 	return (
 		<div className="app">
-			<h1>I&apos;m basic app</h1>
+			<h1>I&apos;m basic app 2</h1>
+			<Counter />
 		</div>
 	);
 }
